017b68531f33c5f6217f1a0c3b6c87c9ba330582
sblrok <sblrok@corpglory.com>
2020-10-02 15:17:07 +0400

    fix codestyle

--- src/routes/tasks.ts
+++ src/routes/tasks.ts
@@ -1,29 +1,37 @@
 import { Target } from '../target'
 
 import * as express from 'express'
-import {Datasource} from "@corpglory/tsdb-kit";
+import { Datasource } from '@corpglory/tsdb-kit';
 
-type DatasourceData = {
-  panelUrl: string,
-  datasourceRequest: Datasource,
-  datasourceName: string
+type TRequest = {
+  body: {
+    from: string,
+    to: string,
+    data: Array<{
+      panelUrl: string,
+      datasourceRequest: Datasource,
+      datasourceName: string
+    }>,
+    target: object,
+    user: string,
+  }
 };
 
-async function addTask(req, res) {
+async function addTask(req: TRequest, res) {
   const body = req.body;
   const from = parseInt(body.from);
   const to = parseInt(body.to);
-  const data: DatasourceData[] = body.data;
+  const data = body.data;
   const targets = [body.target];
   const user = body.user;
 
   if(isNaN(from) || isNaN(to)) {
-    res.status(500).send('Range error: please fill both "from" and "to" fields');
+    res.status(400).send('Range error: please fill both "from" and "to" fields');
   } else if(from >= to) {
-    res.status(500).send('Range error: "from" should be less than "to"');
+    res.status(400).send('Range error: "from" should be less than "to"');
   } else {
     const names = data.map(item => item.datasourceName).join(', ');
-    res.status(200).send('Exporting ' + names + ' data from ' + new Date(from).toLocaleString() + ' to ' + new Date(to).toLocaleString());
+    res.status(200).send(`Exporting ${names} data from ${new Date(from).toLocaleString()} to ${new Date(to).toLocaleString()}`);
 
     data.forEach(request => {
       const target = new Target(request.panelUrl, user, request.datasourceRequest, targets, from, to, request.datasourceName);
