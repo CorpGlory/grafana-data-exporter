63203f27c0e157dee2d2078c64bf9c3f98bf12b5
sblrok <sblrok@corpglory.com>
2020-10-02 04:19:45 +0400

    Changes for single export button support

--- src/routes/tasks.ts
+++ src/routes/tasks.ts
@@ -1,26 +1,34 @@
 import { Target } from '../target'
 
 import * as express from 'express'
+import {Datasource} from "@corpglory/tsdb-kit";
 
+type DatasourceData = {
+  panelUrl: string,
+  datasourceRequest: Datasource,
+  datasourceName: string
+};
 
 async function addTask(req, res) {
-  let body = req.body;
-  let from = parseInt(body.from);
-  let to = parseInt(body.to);
-  let panelUrl = body.panelUrl;
-  let targets = [body.target];
-  let datasource = body.datasourceRequest;
-  let datasourceName = body.datasourceName;
-  let user = body.user;
+  const body = req.body;
+  const from = parseInt(body.from);
+  const to = parseInt(body.to);
+  const data: DatasourceData[] = body.data;
+  const targets = [body.target];
+  const user = body.user;
 
   if(isNaN(from) || isNaN(to)) {
     res.status(500).send('Range error: please fill both "from" and "to" fields');
   } else if(from >= to) {
     res.status(500).send('Range error: "from" should be less than "to"');
   } else {
-    res.status(200).send('Exporting ' + datasourceName + ' data from ' + new Date(from).toLocaleString() + ' to ' + new Date(to).toLocaleString());
-    let target = new Target(panelUrl, user, datasource, targets, from, to, datasourceName);
-    target.export();
+    const names = data.map(item => item.datasourceName).join(', ');
+    res.status(200).send('Exporting ' + names + ' data from ' + new Date(from).toLocaleString() + ' to ' + new Date(to).toLocaleString());
+
+    data.forEach(request => {
+      const target = new Target(request.panelUrl, user, request.datasourceRequest, targets, from, to, request.datasourceName);
+      target.export();
+    });
   }
 }
 
